1ef8b8e48fe5bb74dcd5889feb848055e1f3ab00
rombzg <rom1802@hotmail.fr>
2013-06-16 15:14:12 +0200

    scripts perso

---
+++ scripts/dropbox.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+db=7.4
+ch="$HOME/Dropbox"
+dr=`du -c $ch | grep total | tail -1`
+sz=`echo $dr | sed 's/total//g'`
+if [ -d $ch/.dropbox.cache ]; then
+dc=`du -c $ch/.dropbox.cache | grep total | tail -1`
+sc=`echo $dc | sed 's/total//g'`
+sz=$sz-$sc
+sz=`awk "BEGIN{ print $sz }"`
+fi
+op="100-((100*($db-$sz/1048576))/$db)"
+po=`awk "BEGIN{ print $op }"`
+po=${po%.*}
+echo $po
